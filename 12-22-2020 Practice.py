640ca55d9d7911a25132bbb11781a9a100dc86f5
Matteo <matteo@capasso.dev>
2020-12-22 15:39:04 -0500

    Enter

---
+++ 12-22-2020 Practice.py
@@ -0,0 +1,9 @@
+#Course: Automate the Boring Stuff with Python Programming
+##Section 5: Writing a Complete Program: Guess the Number
+##URL: https://www.udemy.com/course/automate/learn/lecture/3465826?start=0#overview
+
+# Goal: Random Number game. 
+
+import random
+print('what is your name?')
+name = input()
