8119034b64323b06ac649b7b0f0a839e18d3c5a9
Matteo <matteo@capasso.dev>
2020-12-22 16:55:45 -0500

    Enter

--- 12-22-2020 Practice.py
+++ 12-22-2020 Practice.py
@@ -4,6 +4,46 @@
 
 # Goal: Random Number game. 
 
+
 import random
-print('what is your name?')
+
+# Number Inquiry Function Creation (w/ Input Error Checking)
+def inputNumber():
+    validation = input()
+    while validation == ' ':
+        print('You did not enter a number, please try again!')
+        validation = input()
+    
+    return number
+
+# Name inquiry
+print('What is your name?')
 name = input()
+
+# Number Inquiry Text
+print('Hello ' + name +'!')
+print('Guess a number between 0 and 10.')
+inputNumber()
+
+# Random Number Generator Variable Generation
+randomNumber = random.randint(0,10)
+
+
+while number != randomNumber:    
+
+    if number > randomNumber:
+        print('Your number is too high, please try again!')
+        inputNumber()
+
+    elif number < randomNumber:
+        print('Your number is too low, please try again!')
+        inputNumber()
+    else:
+        break
+    
+
+print('Great job, the number was:' + str(randomNumber))
+
+
+
+## This is spaghetti code. Please change to include a limited amount of tries and error validation using truthy and falsey values. - matteo
