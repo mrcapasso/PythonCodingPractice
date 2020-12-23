aed86982d249ba693a38ab2a1b9fa773c08d2295
Matteo <matteo@capasso.dev>
2020-12-23 12:05:55 -0500

    main

--- 12-23-2020 Practice.py
+++ 12-23-2020 Practice.py
@@ -7,12 +7,12 @@
 import random
 
 
-MAXGAMEATTEMPTS = 7
-NUMVALIDATIONATTEMPTS = 3
-LOWERRANDNUMLIMIT = 1
-UPPERRANDNUMLIMIT = 20
+MAX_GAME_ATTEMPTS = 7
+NUM_VALIDATION_ATTEMPTS = 3
+LOW_RAND_NUM_LIM = 1
+UP_RAND_NUM_LIM = 20
 
-randomNumber = random.randint(LOWERRANDNUMLIMIT,UPPERRANDNUMLIMIT)
+randomNumber = random.randint(LOW_RAND_NUM_LIM,UP_RAND_NUM_LIM)
 
 #Name Inquiry
 print('Hello! What is your name?')
@@ -21,16 +21,16 @@ print("Welcome " + name + '.')
 
 
 #Number Inquiry
-for attempts not in range(0,MAXGAMEATTEMPTS):
+for attempts not in range(0,MAX_GAME_ATTEMPTS):
     print("Please input a number between 1 and 20.")
     number = input()
 
     #User Input Validation
-    for number not in range(LOWERRANDNUMLIMIT, UPPERRANDNUMLIMIT):
-        print('Input error, please enter a number between ' str(LOWERRANDNUMLIMIT) +'and ' + str(UPPERRANDNUMLIMIT) +'.')
+    for number not in range(LOW_RAND_NUM_LIM, UP_RAND_NUM_LIM):
+        print('Input error, please enter a number between ' str(LOW_RAND_NUM_LIM) +'and ' + str(UP_RAND_NUM_LIM) +'.')
         number = input()
         numberAttempt = numberAttempt + 1
-        if numberAttempt >= NUMVALIDATIONATTEMPTS:
+        if numberAttempt >= NUM_VALIDATION_ATTEMPTS:
             print("Please learn to read.")
             exit
 
@@ -39,9 +39,9 @@ for attempts not in range(0,MAXGAMEATTEMPTS):
         print("Great job, you guessed correctly!")
         break
     elif number > randomNumber:
-        print('Nice try, but your guess was too high. You have ' + str(MAXGAMEATTEMPTS - attempts) + ' attempts remaining')
+        print('Nice try, but your guess was too high. You have ' + str(MAX_GAME_ATTEMPTS - attempts) + ' attempts remaining')
     else:
-        print('Nice try, but your guess was too low. You have ' + str(MAXGAMEATTEMPTS - attempts) + ' attempts remaining')
+        print('Nice try, but your guess was too low. You have ' + str(MAX_GAME_ATTEMPTS - attempts) + ' attempts remaining')
     attempts = attempts + 1
 
 print("Random number was " + str(randomNumber))
