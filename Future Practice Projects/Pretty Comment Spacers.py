8155ecbd67d32db4fde81369aa9636881b7fbb68
Matteo <matteo@capasso.dev>
2021-01-20 19:01:47 -0500

    Formatting & Function Psuedo

---
+++ Future Practice Projects/Pretty Comment Spacers.py
@@ -0,0 +1,1 @@
+#Goal: Create a program that will generate nice code comment spacers for blocking off coding segments.
--- In-Progress Practice Projects/PhoneAndEmailScraper.py
+++ In-Progress Practice Projects/PhoneAndEmailScraper.py
@@ -2,25 +2,33 @@
 #Tip: Use regex library, pyperclip
 
 ###PsuedoCode###
+
+#Functions:
+    #parse
+        #phone numbers
+        #emails
+    #formatted list printing
+
+#Main Program
 #exit = false
 #Loop Start on exit variable
 #Clipboard pull, paste option, exit?
     #if clipboard
         #Reset clipboard loop's argument
         #Clipboard Loop
-        #retrive clipboard input
-        #paste clipboard sample text in terminal
-        #Desire input?
-            #If yes 
-                #parse
-                #Output to clipboard
-                #also output to terimal in table like format
-                #Continue / More Parsing?
+            #retrive clipboard input
+            #paste clipboard sample text in terminal
+            #Desire input?
                 #if no
-                    #clip board loop exit condition
-                    #main program exit condition
-            #if no
-                #please try again text, restarting
+                        #please try again text, restarting
+                #If yes 
+                    #parse
+                    #Output to clipboard
+                    #also output to terimal in table like format
+                    #Continue / More Parsing?
+                    #if no
+                        #clip board loop exit condition
+                        #main program exit condition
     #elif paste option
         #clear terminal
         #user input string
