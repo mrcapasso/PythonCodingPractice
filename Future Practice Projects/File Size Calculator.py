ece91ce958c4489fb6926603c06ec5febd5c3434
Matteo <matteo@capasso.dev>
2021-02-01 12:04:29 -0500

    Updated sources for potential ideas

--- Future Practice Projects/File Size Calculator.py
+++ Future Practice Projects/File Size Calculator.py
@@ -1,3 +1,3 @@
 #Goal: Create a program that tells you how large a folder by adding up it's file sizes. Be sure to include the size in gigabytes, kilobytes, and megabytes. 
 #Tip: Use os module
-#See 15:31 to 17:34 of https://www.udemy.com/course/automate/learn/lecture/3470538#notes for solution. 
+#Idea Source: See 15:31 to 17:34 of https://www.udemy.com/course/automate/learn/lecture/3470538#notes
---
+++ Future Practice Projects/Future Projects Master List.md
@@ -0,0 +1,4 @@
+Potential Practice Project Idea Sources
+https://github.com/karan/Projects/
+https://www.dreamincode.net/forums/topic/78802-martyr2s-mega-project-ideas-list/
+https://www.amazon.com/Python-Crash-Course-Hands-Project-Based/dp/1593276036
