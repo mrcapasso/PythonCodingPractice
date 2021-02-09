046c6c0dc52c842000261bffe6af2d73bd0284b2
Matteo <matteo@capasso.dev>
2021-02-09 16:50:26 -0500

    WIP, partly fixed justifications

--- In-Progress Practice Projects/Psuedocode and Temp.py
+++ In-Progress Practice Projects/Psuedocode and Temp.py
@@ -1,6 +1,8 @@
 #Potential Features List:
 #Option for Custom Character String Pattern
 #Option to center, left justify, right justify
+#Option for symmetry, think of dummy variables to simplify code
+#Optimize Run-Time by replacing for loop in patternFormatter()
 
 
 #Psuedocode
@@ -19,35 +21,39 @@
         #if no
             #exit loop 1
 
-
-##Think of New Method b/c .center() only allows one character at a time
-    #Use For Loop to pass through various values in string
+#############################################################################################################################(function testing)#############################################################################################################################            
 
 import os, pyperclip
 
-def patternFormatter(pattern, justification, length):
-#Function Input Pre-Check
+def patternFormatter(pattern, text, justification, length):
+    #Function Input Pre-Check
     if justification not in ['center','left','right']:
         raise AssertionError('Improper Justification Option')
     if length <= 0 or (length%1 != 0): #Checking for positive integer.
         raise AssertionError('Improper Length Option')
     if pattern == None:
         raise AssertionError('Improper Pattern') 
+    #*! Create Assertion to validate proper len of text and pattern
     #Function Code
+    text.strip() #*? Not working? 
+    text = '(' + text + ')'
+    finalString = text
     if justification == 'center':
-        pattern.strip()
-        pattern.center(1, ' ')
-        finalString = pattern.center(length, pattern)
+        for i in range(0, round(length/2)): #*! Modify upper limit to adjust accordingly
+            finalString = pattern + finalString + pattern[::-1]
+            #Reversed pattern is important for maintaining symmetry
         return finalString
     elif justification == 'left':
-        pattern.strip()
-        pattern.center(1, ' ')
-        finalString = pattern.ljust(length, pattern)
+        finalString = '#' + finalString #Specific to left justified, mean't to prevent errors in compiling.
+        for i in range(0, length): #*! Modify upper limit to adjust accordingly
+            finalString = finalString + pattern[::-1]
         return finalString
     else: #Right Justified Option
-        pattern.strip()
-        pattern.center(1, ' ')
-        finalString = pattern.rjust(length, pattern)
+        for i in range(0, round(((length-len(text))/len(pattern)))): #*! Modify upper limit to adjust accordingly
+            finalString = pattern + finalString
         return finalString
 
-print(patternFormatter('#*!', 'center', 30))
+a = patternFormatter('#', 'function testing', 'center', 250)
+os.system('cls')
+print(a)
+print(len(a))
