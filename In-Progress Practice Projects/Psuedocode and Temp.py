3b0a207a5537c0de92f69b98202cabce56aec210
Matteo <matteo@capasso.dev>
2021-02-09 13:52:59 -0500

    WIP, creating patternFormatter()

--- In-Progress Practice Projects/Psuedocode and Temp.py
+++ In-Progress Practice Projects/Psuedocode and Temp.py
@@ -5,11 +5,8 @@
 
 #Psuedocode
 #function: Pattern Display
-#function: Format String 
 
 
-# -! you are here
-        #Strip Text
         #Text + Pattern String Function
         #Print String (Small Size, medium size, large size)
         #pick desired string
@@ -21,3 +18,36 @@
     #Format another string? 
         #if no
             #exit loop 1
+
+
+##Think of New Method b/c .center() only allows one character at a time
+    #Use For Loop to pass through various values in string
+
+import os, pyperclip
+
+def patternFormatter(pattern, justification, length):
+#Function Input Pre-Check
+    if justification not in ['center','left','right']:
+        raise AssertionError('Improper Justification Option')
+    if length <= 0 or (length%1 != 0): #Checking for positive integer.
+        raise AssertionError('Improper Length Option')
+    if pattern == None:
+        raise AssertionError('Improper Pattern') 
+    #Function Code
+    if justification == 'center':
+        pattern.strip()
+        pattern.center(1, ' ')
+        finalString = pattern.center(length, pattern)
+        return finalString
+    elif justification == 'left':
+        pattern.strip()
+        pattern.center(1, ' ')
+        finalString = pattern.ljust(length, pattern)
+        return finalString
+    else: #Right Justified Option
+        pattern.strip()
+        pattern.center(1, ' ')
+        finalString = pattern.rjust(length, pattern)
+        return finalString
+
+print(patternFormatter('#*!', 'center', 30))
