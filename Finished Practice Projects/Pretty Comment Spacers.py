b20c79f96daf4d1eb21b875662b09f41b0a31fe3
Matteo Capasso <mcapass2@binghamton.edu>
2021-02-12 18:25:05 -0800

    Added patternFormatter() & documentation

--- Finished Practice Projects/Pretty Comment Spacers.py
+++ Finished Practice Projects/Pretty Comment Spacers.py
@@ -1,6 +1,9 @@
-import os, pyperclip
+#Author: Matteo Capasso
+#Goal: Create a program that will generate nice code comment spacers for blocking off coding segments.
 
-def patternFormatter(pattern, text, justification, length):
+import os, pyperclip, pprint
+
+def patternFormatter(pattern, text, justification, length): #Outputs formatted string of text and pattern.
     #Parameter pre-check -- Start
     if str(pattern) != str(pattern): 
         #Pattern parameter type pre-check.
@@ -67,19 +70,70 @@ def patternFormatter(pattern, text, justification, length):
         return finalString
     #Right Justifying Portion -- End
 
-print(patternFormatter(234 , 456, 'center', '80'))
+patterns = {'0':'Custom Pattern','1':'#!', '2':'#?', '3':'#'}
 
-#Stress Test, Checking Conditions
-# import random
-# randomIndex = random.randint(1,100)
-# randomIndexSmall = random.randint(0,3)
-# justification = ['right', 'center', 'left']
+exit = False
+while exit == False:
+    #Welcome Screen & Pattern Option Selection -- Start
+    os.system('cls')
+    print('\nWelcome to the pretty comment spacers script.\n\n' + '-'*80)
+    pprint.pprint(patterns, width=1)
+    print('-'*80 + "\n" + 'Please choose the number corresponding to your desired pattern.')
+    patternNum = input('\nNumber: ')
+    #Welcome Screen & Pattern Option Selection -- End
 
-# patternList = []
-# for i in range(1,101):
-#     patternList.append('#*!'*i)
+    #User's Pattern Input Validation -- Start
+    patternsKeyList = []
+    for keys in patterns.keys():
+        patternsKeyList.append(keys)
+    if patternNum not in patternsKeyList:
+        raise Exception('Invalid Pattern Selection Number')
+    #User's Pattern Input Validation -- End
+    
+    #Special Conditionality Handling (Option 0) -- Start
+    if int(patternNum) == 0:
+        patternType = input('Please enter you desired pattern: ')
+    else:
+        patternType = patterns.get(patternNum)
+    #Special Conditionality Handling (Option 0) -- End
 
-# for i in range(1,100):
-#     print(
-#         patternFormatter(patternList[randomIndex], 'Section', 'center', 80)
-#       )
+    #Clipboard Text Retrival -- Start
+    os.system('cls')
+    print('''
+    Please take this oppurtunity to copy 
+    the single line of text you would 
+    like modifiyed to your clipboard.
+    ''')
+    os.system('pause')
+    os.system('cls')
+    clipboard = pyperclip.paste()
+    clipboard.strip()
+    print(clipboard + "\n")
+    #Clipboard Text Retrival -- End
+
+    #Clipboard Input Validation & Text Justification Retrival -- Start
+    verfication = input('Is this the text you want formatted? (yes/no): ')
+    if verfication.lower() == 'no':
+        clipboard = input('\nType the text you want formatted: ')
+    os.system('cls')
+    justOptions = ['left', 'center', 'right']
+    justification = input('Please choose a text justification.\n' + str(justOptions) + ': ')
+    os.system('cls')
+    if justification not in justOptions:
+        raise Exception('Not a valid justification option.')
+    #Clipboard Input Validation & Text Justification Retrival -- End
+
+    #Formatted Text Print and Copying to Clipboard -- Start
+    standardOption = patternFormatter(patternType, clipboard, justification, 80)
+    pyperclip.copy(standardOption)
+    print(
+        'Small):\n' + patternFormatter(patternType, clipboard, justification, 40) + "\n" + 
+        'Standard):\n' + standardOption + "\n" +
+        'Large):\n' + patternFormatter(patternType, clipboard, justification, 120) + "\n"*2 +
+        'Note: The standard option was copied to your clipboard by default.\n'
+    )
+    os.system('pause')
+    #Formatted Text Print and Copying to Clipboard -- End
+
+    if input('Would you like to exit the program? (yes/no): ').lower() == 'yes':
+        exit = True
