c2620f5f0ffefa93270c93e487007f44c1b88faf
Matteo Capasso <mcapass2@binghamton.edu>
2021-02-18 06:06:22 -0800

    Added new practice project workstations

---
+++ WIP Practice Projects/Open Source Essentials.py
@@ -0,0 +1,31 @@
+# Tabular web dashboard containing links to essential open source work.
+# Ideal Features:
+
+#     Auto-Updating link to current version.
+#     Listed licensed variant alternative. (ex: 7-zip vs WinRaR)
+#     Note page to exmplain pro/con to each item.
+#     Student Variant: Link to current coupons/variants provided to students. (ex: github for students)
+
+# Programs
+
+#     Firefox
+#     SumatraPDF
+#     GIMP
+#     Libreoffice
+#     Ubuntu
+#     VSCode
+
+# Project Managment (requires research)
+
+#     Looking for Kanban and Gantt Chart
+
+#     Taiga: http://taigaio.github.io/taiga-doc/dist/setup-production.html
+#     OFBiz (Enterprise resource planning (ERP): https://ofbiz.apache.org/
+#     Oboo (Enterprise resource planning (ERP): https://www.odoo.com/page/project-management
+#     Gitlab: https://about.gitlab.com/stages-devops-lifecycle/issueboard/
+#     Kanban: https://kanboard.org/
+
+# Accounting
+
+#     GnuCash: https://www.gnucash.org/
+#     TurboCASH: https://www.turbocash.net/
